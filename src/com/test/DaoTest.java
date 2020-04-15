03107ae5ed0b66aeae11a5087de04cecc7260819
ChaosAlphard <Alphard450@outlook.com>
2020-04-16 00:23:45 +0800

    测试更新

--- src/com/test/DaoTest.java
+++ src/com/test/DaoTest.java
@@ -1,21 +1,37 @@
 package com.test;
 
-import com.common.ConnSQL;
-import com.dao.*;
-import com.model.*;
+import com.dao.BarDao;
+import com.dao.UserDao;
+import com.model.Bar;
+import com.model.User;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 public class DaoTest {
     public static void main(String[] args){
         DaoTest test = new DaoTest();
+
         test.isBarExist("Java");
+//        for(int i=0; i<20; i++) {
+//            new Thread(() -> test.findByName("a")).start();
+//        }
     }
 
     private void isBarExist(String name) {
         BarDao dao = new BarDao();
         System.out.println(dao.isBarExist(name));
+    }
+
+    private void findByName(String name) {
+        BarDao dao = new BarDao();
+        final List<Bar> lis = dao.FindBars(name);
+        lis.forEach(it -> System.out.println(it.toString()));
+    }
+
+    private void checkUserLv(int id) {
+        UserDao dao = new UserDao();
+        final User byID = dao.findByID(id);
+        System.out.println(byID.toString());
+        System.out.println(dao.checkUserLV(id));
     }
 }
